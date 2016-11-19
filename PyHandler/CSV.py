8540764d35fdda9a519a10fd000c5d6b194ae09a
Vishak KC <Vishak KC>
2016-11-19 12:38:55 +0530

    1 cluser

--- PyHandler/CSV.py
+++ PyHandler/CSV.py
@@ -1,15 +1,13 @@
 import csv
 import sys
 mergefile  = open('mergefile.csv', "wt",encoding="utf8")
-writer = csv.writer(mergefile, quotechar='"', quoting=csv.QUOTE_ALL)
-for x in range(2,6):
+writer = csv.writer(mergefile,delimiter=',',quotechar='"', quoting=csv.QUOTE_ALL)
+for x in range(1,6):
     q = 'dtm'+str(x)+'.csv'
     with open(q, 'rt',encoding="utf8") as f:
         reader = csv.reader(f)
-        e=0
         for row in reader:
             writer.writerow(row)
-            print(q,e)
 mergefile.close()
 
 """f = open(sys.argv[1], 'rb') # opens the csv file
